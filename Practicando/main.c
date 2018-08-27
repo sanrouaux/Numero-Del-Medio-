b3c02db10fc314a8b9467b17aa42d27e1d657291
alumno <alumno>
2018-08-27 14:45:14 -0300

    Pido los tres numeros

---
+++ Practicando/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int numUno;
+    int numDos;
+    int numTres;
+    int maximo;
+    int minimo;
+    int numeroMedio;
+
+
+    printf("Ingrese el primer numero:\n");
+    scanf("%d",&numUno);
+
+
+    printf("Ingrese el segundo numero:\n");
+    scanf("%d",&numDos);
+
+    if(numDos)
+
+    printf("Ingrese el tercer numero:\n");
+    scanf("%d",&numTres);
+
+    printf("%d",numUno);
+
+    return 0;
+}
